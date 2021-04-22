227c3c460d3a7e8d0091722305351ba2cc078dbb
svasenkov <aanher@gmail.com>
2021-04-22 13:10:47 +0300

    updated code for selenoid run

--- src/test/java/tests/AppiumTestBase.java
+++ src/test/java/tests/AppiumTestBase.java
@@ -1,8 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import drivers.AppiumMobileDriver;
-import drivers.BrowserstackMobileDriver;
+import drivers.SelenoidMobileDriver;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -14,7 +13,6 @@ import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
-import static helpers.BrowserstackHelper.getBSPublicLink;
 import static io.qameta.allure.Allure.step;
 
 public class AppiumTestBase {
@@ -22,25 +20,32 @@ public class AppiumTestBase {
     @BeforeAll
     public static void beforeAll() {
         addListener("AllureSelenide", new AllureSelenide());
-        Configuration.browser = AppiumMobileDriver.class.getName();
+        Configuration.browser = SelenoidMobileDriver.class.getName();
         Configuration.startMaximized = false;
         Configuration.browserSize = null;
         Configuration.timeout = 10000;
+
+        System.setProperty("selenoid.url", "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub");
+        System.setProperty("video.url", "https://selenoid.autotests.cloud/video/");
+    }
+
+    public static String getSessionId() {
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 
     @BeforeEach
     void startDriver() {
-        step("Open application", ()-> open());
+        step("Open application", () -> open());
     }
 
     @AfterEach
     public void afterEach() {
+        String sessionId = getSessionId();
+
         attachScreenshot("Last screenshot");
         attachPageSource();
+        if (System.getProperty("video.url") != "")
+            attachVideo(sessionId);
         closeWebDriver();
-    }
-
-    public static String getSessionId(){
-        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 }
